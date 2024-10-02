2c58bb829a417946430a3078a44c8c486d63eade
Maryna Kucher <maryna.dev.k@gmail.com>
2024-10-02 13:04:54 +0300

    Navbar is created

---
+++ app/NavBar/NavBar.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import React from 'react';
+import NavLinks from './NavLinks';
+import { PiBugThin } from "react-icons/pi";
+import { Container, Flex } from '@radix-ui/themes';
+
+const NavBar = () => {
+  return (
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <PiBugThin />
+            </Link>
+            <NavLinks />
+          </Flex>
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ app/NavBar/NavLinks.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Link from 'next/link';
+import classnames from 'classnames';
+import { usePathname } from 'next/navigation';
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues/list' },
+  ];
+
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classnames({
+              'nav-link': true,
+              '!text-teal-900, bg-teal-100': link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavLinks;
